4e26c52c6f072b80985cf9f95867abd97208c3c7
Rootshell <bcairns@voidrealms.com>
2017-12-08 11:21:54 -0500

    first comment

---
+++ helloflutter/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Center(
+      child: new MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Hello Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(title: new Text('Hello Flutter'),),
+        body: new Center(
+          child: new RaisedButton(
+            onPressed: null,
+            child: new Text('My Button'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+Code for video tutorial
+
+Title: Flutter 20 - MaterialApp
+Published: 12/8/2017 11:30 AM
+Link: https://youtu.be/1_N03O0aFw0
+
+Description: Learn how to use the MaterialApp
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
